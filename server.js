67d37c87f4eafd39c99bbe11f8c7a11344df9709
Sreyashipaul <67593254+Sreyashipaul@users.noreply.github.com>
2021-02-05 15:32:23 +0600

    server update

--- server.js
+++ server.js
@@ -1,52 +1,55 @@
 
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
-const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectId;
-require('dotenv').config();
-
+require("dotenv").config();
 
 const app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const uri =
+  "mongodb+srv://9t6:livelovedie@cluster0.dupvj.mongodb.net/volunteer-network?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const port = 5000;
 
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sw5ft.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true ,useUnifiedTopology: true,});
-client.connect(err => {
-    const tasks = client.db("volunteer-network").collection("tasks");
-   const registrations = client.db("volunteer-network").collection("registrations");
-    
-      app.get("/tasks", (req, res) => {
-        tasks.find({}).toArray((err, documents) => {
-          res.send(documents);
-        });
-      });
-      app.post("/addRegistration", (req, res) => {
-        const activity = req.body;
-        registrations.insertOne(activity).then((result) => {
-          res.send(result);
-          console.log(result);
-        });
-      });
-    
-      app.get("/activities", (req, res) => {
-        const activity = req.body;
-        activities.find({ email: req.query.email }).toArray((err, documents) => {
-          res.send(documents);
-        });
-      });
-}); 
-      
+client.connect((err) => {
+  const tasks = client.db("volunteer-network").collection("tasks");
 
+  const registrations = client
+    .db("volunteer-network")
+    .collection("registrations");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-    
-app.listen (process.env.PORT|| port);
+  app.get("/tasks", (req, res) => {
+    tasks.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+  app.post("/addRegistration", (req, res) => {
+    const activity = req.body;
+    registrations.insertOne(activity).then((result) => {
+      res.send(result);
+      console.log(result);
+    });
+  });
+
+  app.get("/activities", (req, res) => {
+    const activity = req.body;
+    activities.find({ email: req.query.email }).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT || port);
